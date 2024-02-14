c8244f1d37b237c5696df4b97988e703028dffd2
SherMuhammadgithub <muhammadiqbalshermuhammad@gmail.com>
2024-02-15 00:08:51 +0500

    implemented the user driver

--- Books Management( Data Frames + member functions)/Books Management( Data Frames + member functions)/BookStore.cs
+++ Books Management( Data Frames + member functions)/Books Management( Data Frames + member functions)/BookStore.cs
@@ -8,6 +8,63 @@ namespace Books_Management__Data_Frames___member_functions_
 {
     internal class BookStore
     {
+        private BoooksRepositry books_repo;
+        private MemberRepositry memeber_repo;
 
+        public BookStore()
+        {
+            books_repo = new BoooksRepositry();
+            memeber_repo = new MemberRepositry();
+        }
+
+        public void AddBook(Book book)
+        {
+            books_repo.AddBook(book);
+        }
+
+        public Book SearchBookByTitle(string title) {
+            return books_repo.SearchBookByTitle(title);
+        }
+        public Book SearchBookById(int id)
+        {
+            return books_repo.SearchBookByISBN(id);
+        }
+        public bool UpdateBook(int id, Book updated_book)
+        {
+            return books_repo.UpdateBookStock(id, updated_book);
+        }
+        // ****Member Functions****
+        public void AddMember(Member member)
+        {
+            memeber_repo.AddMember(member);
+        }
+        public Member SearchMember(string search_query)
+        {
+            return memeber_repo.GetMember(search_query);
+        }
+
+        public bool UpdateMemberInfo(string search_query, Member updated_member)
+        {
+            return memeber_repo.UpdateMemberInfo(search_query, updated_member);
+        }
+
+        public bool PurchaseBook(string member_name, bool isMember, int book_ISBN)
+        {
+            Book book = books_repo.SearchBookByISBN(book_ISBN);
+            Member member = memeber_repo.GetMember(member_name);
+
+            if (isMember)
+            {
+                member.money_in_bank-= (book.price*5)/100;
+                return true;
+            }
+            else if(!isMember)
+            {
+                member.money_in_bank -= book.price;
+                return true;
+                
+            }
+            return false;
+        }
     }
 }
--- Books Management( Data Frames + member functions)/Books Management( Data Frames + member functions)/Program.cs
+++ Books Management( Data Frames + member functions)/Books Management( Data Frames + member functions)/Program.cs
@@ -8,8 +8,52 @@ namespace Books_Management__Data_Frames___member_functions_
 {
     internal class Program
     {
+
+        enum MenuOption
+        {
+            AddBook = 1,
+            SearchBookByISBN,
+            SearchBookByTitle,
+            UpdateStock,
+            AddMember,
+            SearchMember,
+            UpdateMemberInfo,
+            PurchaseBook,
+            Exit
+        }
+
         static void Main(string[] args)
         {
+            BookStore bookStore = new BookStore();
+            Console.WriteLine("****BookStore****");
+            while(true)
+            {
+                MenuOption choice = UserMenu();
+                if(choice == MenuOption.AddBook)
+                {
+
+                }
+            
+                }
+        }
+        static MenuOption UserMenu()
+        {
+            Console.WriteLine("1.Add a Book");
+            Console.WriteLine("2.Search for a Book by ISBN");
+            Console.WriteLine("3.Search for a Book by Title");
+            Console.WriteLine("4.Update Stock of a Book");
+            Console.WriteLine("5.Add a Member");
+            Console.WriteLine("6.Search for a Member by Name or ID");
+            Console.WriteLine("7.Update Member Information");
+            Console.WriteLine("8.Purchase a Book");
+            Console.WriteLine("9.Exit");
+            int option;
+            if(!int.TryParse(Console.ReadLine(), out option) || option < 1 || option > 9 )
+            {
+                UserMenu();
+            }
+            return (MenuOption)option;
+
         }
     }
 }
